02df41a6f3d8caaab5e441acfe0fdeec155d45b4
Yogesh Muthuraj <Yogesh.Muthuraj@cerner.com>
2018-10-26 01:18:46 +0530

    o

--- custom_pgms/dc_motor.py
+++ custom_pgms/dc_motor.py
@@ -12,11 +12,11 @@ p.start(7.5)
 try:
     while True:
         p.ChangeDutyCycle(7.5)  # turn towards 90 degree
-        time.sleep(1) # sleep 1 second
+        time.sleep(5) # sleep 1 second
         p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-        time.sleep(1) # sleep 1 second
+        time.sleep(5) # sleep 1 second
         p.ChangeDutyCycle(12.5) # turn towards 180 degree
-        time.sleep(1) # sleep 1 second
+        time.sleep(5) # sleep 1 second
 except KeyboardInterrupt:
     p.stop()
     GPIO.cleanup()
