d7820faa3f7697e7ddc6caeaf29a0981148110cc
Yogesh Muthuraj <Yogesh.Muthuraj@cerner.com>
2018-10-25 15:56:15 +0530

    o

--- custom_pgms/interactive.py
+++ custom_pgms/interactive.py
@@ -13,30 +13,27 @@ GPIO.setmode(GPIO.BCM)
 #for defining more than 1 GPIO channel as input/output use
 GPIO.setup(motor_channel, GPIO.OUT)
 
-motor_direction = input('select motor direction a=anticlockwise, c=clockwise: ')
 while True:
     try:
-        if(motor_direction == 'c'):
-            print('motor running clockwise\n')
-            GPIO.output(motor_channel, (GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.HIGH,GPIO.HIGH,GPIO.LOW,GPIO.LOW))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.LOW,GPIO.LOW,GPIO.HIGH,GPIO.HIGH))
-            sleep(0.02)
+        print('motor running clockwise\n')
+        GPIO.output(motor_channel, (GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH))
+        sleep(0.02)
+        GPIO.output(motor_channel, (GPIO.HIGH,GPIO.HIGH,GPIO.LOW,GPIO.LOW))
+        sleep(0.02)
+        GPIO.output(motor_channel, (GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW))
+        sleep(0.02)
+        GPIO.output(motor_channel, (GPIO.LOW,GPIO.LOW,GPIO.HIGH,GPIO.HIGH))
+        sleep(0.02)
 
-        elif(motor_direction == 'a'):
-            print('motor running anti-clockwise\n')
-            GPIO.output(motor_channel, (GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.LOW,GPIO.LOW,GPIO.HIGH,GPIO.HIGH))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW))
-            sleep(0.02)
-            GPIO.output(motor_channel, (GPIO.HIGH,GPIO.HIGH,GPIO.LOW,GPIO.LOW))
-            sleep(0.02)
+        print('motor running anti-clockwise\n')
+        GPIO.output(motor_channel, (GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH))
+        sleep(0.02)
+        GPIO.output(motor_channel, (GPIO.LOW,GPIO.LOW,GPIO.HIGH,GPIO.HIGH))
+        sleep(0.02)
+        GPIO.output(motor_channel, (GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW))
+        sleep(0.02)
+        GPIO.output(motor_channel, (GPIO.HIGH,GPIO.HIGH,GPIO.LOW,GPIO.LOW))
+        sleep(0.02)
 
 
     #press ctrl+c for keyboard interrupt
