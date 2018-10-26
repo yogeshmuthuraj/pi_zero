5eaaf063e23f9b2308aec30b336ec52bedbbf735
Yogesh Muthuraj <Yogesh.Muthuraj@cerner.com>
2018-10-26 08:34:08 +0530

    o

--- custom_pgms/dc_motor.py
+++ custom_pgms/dc_motor.py
@@ -4,7 +4,6 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(12, GPIO.OUT)
-GPIO.setup(38, GPIO.OUT)
 
 p = GPIO.PWM(12, 50)
 
@@ -12,24 +11,10 @@ p.start(7.5)
 
 try:
     while True:
-        GPIO.output(38, True)
         p.ChangeDutyCycle(5)
-        time.sleep(2)
-        GPIO.output(38, False)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(2)
-        p.ChangeDutyCycle(10)
-        time.sleep(2)
-        p.ChangeDutyCycle(12.5)
-        time.sleep(2)
-        p.ChangeDutyCycle(10)
-        time.sleep(2)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(2)
-        p.ChangeDutyCycle(5)
-        time.sleep(2)
+        time.sleep(1)
         p.ChangeDutyCycle(2.5)
-        time.sleep(2)
+        time.sleep(1)
 except KeyboardInterrupt:
     p.stop()
     GPIO.cleanup()
